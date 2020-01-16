8dad6b0501b86db2d2d36420deeabdcb4019c480
Johannes Englisch <johannes.englisch@uni-leipzig.de>
2020-01-16 10:10:20 +0100

    only show authors in Contributors data table

--- crossgram/datatables.py
+++ crossgram/datatables.py
@@ -1,2 +1,24 @@
+from clld.db.meta import DBSession
+from clld.db.models import common
+
+from clld.web import datatables
+from clld.web.datatables.base import DataTable
+from clld.web.datatables.contributor import NameCol, ContributionsCol, AddressCol
+
+
+class ContributionContributors(DataTable):
+    def base_query(self, query):
+        return DBSession.query(common.Contributor) \
+            .join(common.ContributionContributor) \
+            .join(common.Contribution)
+
+    def col_defs(self):
+        return [
+            NameCol(self, 'name'),
+            ContributionsCol(self, 'Contributions'),
+            AddressCol(self, 'address'),
+        ]
+
+
 def includeme(config):
-    pass
+    config.register_datatable('contributors', ContributionContributors)
