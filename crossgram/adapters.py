cbc29706eddf191496ec387646de2173cd8a6c9e
Johannes Englisch <johannes_englisch@eva.mpg.de>
2023-11-13 13:17:47 +0100

    lparameter map: use language names as labels for when a parameter has a closed set of codes

--- crossgram/adapters.py
+++ crossgram/adapters.py
@@ -1,2 +1,20 @@
+from clld.web.adapters.geojson import GeoJsonParameter
+from clld import interfaces
+
+
+class GeoJsonLParameter(GeoJsonParameter):
+
+    def feature_properties(self, ctx, req, valueset):
+        # use language names as tool tips for parameters with a closed domain
+        if getattr(ctx, 'domain', None):
+            label = self.get_language(ctx, req, valueset).name
+        else:
+            label = ', '.join(v.name for v in valueset.values if v.name)
+        return {
+            'values': list(valueset.values),
+            'label': label}
+
+
 def includeme(config):
-    pass
+    config.register_adapter(
+        GeoJsonLParameter, interfaces.IParameter, interfaces.IRepresentation)
